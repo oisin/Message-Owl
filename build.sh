64173044f5e60af776b6b28f4332802379d16187
ohurley <oisin.hurley@gmail.com>
2010-02-16 13:41:18 +0000

    - renamed the build feature to message_owl - added a parameterized build feature to build scripts - updated RMAP to find renamed build feature

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source=`pwd`
-build_feature=org.fusesource.tools.messaging.build
+build_feature=message_owl
 
 # Update these properties to point to the locations of your Eclipse JEE package
 # install, your Orbit install (if you have one) and the directory you want the
@@ -9,12 +9,19 @@ build_feature=org.fusesource.tools.messaging.build
 platform=${source}/platform/eclipse
 orbit=${source}/orbit
 build=${source}/build
+debug=
+clean=no
+force=i
 
 while [ $# -gt 0 ] ; do
   case $1 in
     -p) shift; platform=$1 ;;
     -b) shift; orbit=$1 ;;
     -o) shift; build=$1 ;;
+    -f) shift; build_feature=$1;;
+    -d) debug="-L DEBUG" ;;
+    -c) clean=yes ;;
+    -fc) clean=yes; force=f ;;
      *) echo option $1 ignored ;;
   esac;
   shift
@@ -26,7 +33,19 @@ done
 buckminster=
 
 # This is the line that kicks it all off
-${buckminster}buckminster -data ${build} -L DEBUG -S ${source}/org.fusesource.tools.messaging.build/buckminster.script -vmargs -Dsource=${source} -Dplatform=${platform} -Dorbit=${orbit} -Dbuild=${build}
+if [ ${clean} = no ]
+then
+  ${buckminster}buckminster -data ${build} ${debug} -S ${source}/${build_feature}/buckminster.script -vmargs -Dsource=${source} -Dplatform=${platform} -Dorbit=${orbit} -Dbuild=${build} -Dfeature=${build_feature}
+  if [ -f ${build}/output/${build_feature}*/*.zip ]
+  then
+    mv ${build}/output/${build_feature}*/*.zip ${build} && echo "Zipped p2 update site is available in ${build} directory"
+  fi
+else
+   echo "Cleaning..."
+   if [ ! -z ${build} -a -w ${build} -a ${build} != "/" ]
+   then
+     echo "Deleting old workspace at ${build}..."
+     rm -r${force} ${build}
+   fi
+fi
 
-mv ${build}/output/${build_feature}*/*.zip ${build}
-echo "Zipped p2 update site is available in ${build} directory"
